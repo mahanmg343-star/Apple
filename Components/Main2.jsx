acba923cd55319aec11d2ce635ecbef1b9c54e4e
Mahan M.G <mahanmg343@gmail.com>
2026-08-03 21:31:37 +0330

    Initial commit

---
+++ Components/Main2.jsx
@@ -0,0 +1,416 @@
+
+import sp from '../assets/smainp.jpg'
+import fp from '../assets/fmainp.jpg'
+import tp from '../assets/thmainp.jpg'
+import ff from '../assets/ffmainp.jpg'
+import fff from '../assets/fffmainp.jpg'
+import ss from '../assets/ssmainp.jpg'
+import sss from '../assets/sssmainp.jpg'
+import e from '../assets/emainp.jpg'
+import n from '../assets/nmainp.jpg'
+
+function main() {
+  return (
+    <>
+
+
+<section className="container-fluid">
+  <section className="flex flex-wrap">
+
+<div className="relative w-full overflow-hidden">
+
+    <a href="https://www.apple.com/us/shop/goto/edu_store">
+
+    <img src={fp} className="w-full h-100 object-cover  flex mt-17 " />
+    </a>
+   
+    <a href="https://www.apple.com/us/shop/goto/edu_store">
+
+    <div className=''>
+
+<span className='absolute flex top-20 justify-center left-0 right-1
+                  text-4xl font-bold
+'>College,sorted.</span>
+
+<span className='absolute flex top-30  justify-center left-0 right-1
+                  text-xl 
+'>Get a gift card from$100 to $150*   
+when you buy Mac or iPad
+ with eduction savings.
+ </span>
+    <button className="absolute flex top-45 right-1/2  
+                   bg-blue-500 hover:bg-blue-400 
+                   text-white font-bold py-2 px-4 rounded-3xl 
+                   shadow-sm transition duration-200">
+        Shop
+    </button>
+    
+    </div>
+</a>
+
+</div>
+
+
+
+<div className="relative  w-full overflow-hidden">
+
+    <a href="https://www.apple.com/macbook-air/">
+
+    <img src={sp} className="w-full h-100 object-cover  flex mt-2" />
+    </a>
+   
+<a href="https://www.apple.com/macbook-air/">
+
+    <div className=''>
+
+<span className='absolute flex top-9 justify-center left-0 right-1
+                  text-4xl font-bold
+'>MacBook Air</span>
+
+<span className='absolute flex top-20 items-center justify-center left-0 right-1
+                  text-xl 
+'>Now superccharged by M5</span>
+    <button className="absolute flex top-30 justify-center right-1/2
+                   bg-blue-500 hover:bg-blue-400 
+                   text-white font-bold py-2 px-4 rounded-3xl 
+                   shadow-lg transition duration-300">
+        Learn more
+    </button>
+    <button className="absolute top-30 justify-center left-1/2 
+                   bg-white hover:bg-blue-500 hover:text-white
+                   text-blue-500 font-bold py-2 px-4 rounded-3xl round-
+                   shadow-sm transition duration-200">
+        Buy
+    </button>
+    </div>
+</a>
+
+</div>
+
+<div className="relative  w-full overflow-hidden">
+
+    <a href="https://www.apple.com/iphone/">
+
+    <img src={tp} className="w-full h-100 object-cover  flex mt-2" />
+    </a>
+   
+    <a href="https://www.apple.com/iphone/">
+
+    <div className=''>
+
+<span className='absolute flex top-9 justify-center left-0 right-1
+                  text-4xl font-bold
+'>iPhone</span>
+
+<span className='absolute flex top-20 items-center justify-center left-0 right-1
+                  text-xl 
+'>Meet the latest iPhone lineup.</span>
+    <button className="absolute flex top-30 justify-center right-1/2
+                   bg-blue-500 hover:bg-blue-400 
+                   text-white font-bold py-2 px-4 rounded-3xl 
+                   shadow-sm transition duration-300">
+        Learn more
+    </button>
+    <button className="absolute top-30 justify-center left-1/2 
+                   bg-white hover:bg-blue-500 hover:text-white
+                   text-blue-500 font-bold py-2 px-4 rounded-3xl round-
+                   shadow-sm transition duration-200">
+        Shop iPhone
+    </button>
+    </div>
+</a>
+
+</div>
+
+      </section>
+    </section>
+
+<section className='container-fluid'>
+    <section className='flex flex-wrap'>
+
+
+
+<div className="relative md:w-6/12 w-full overflow-hidden">
+
+    <a href="https://www.apple.com/us/shop/goto/apple_upgrade">
+
+    <img src={ff} className="w-full h-100 object-cover  flex mt-2" />
+    </a>
+   
+    <a href="https://www.apple.com/us/shop/goto/apple_upgrade">
+
+    <div className=''>
+
+<span className='absolute flex top-9 justify-center left-0 right-1
+                  text-4xl font-bold
+'><box-icon type='logo' name='apple'></box-icon>
+
+    Upgrade</span>
+
+<span className='absolute flex top-20 items-center justify-center left-0 right-1
+                  text-xl 
+'>Love it.Lease it.Upgrade it.</span>
+    <button className="absolute flex top-30 justify-center right-1/2
+                   bg-blue-500 hover:bg-blue-400 
+                   text-white font-bold py-2 px-4 rounded-3xl 
+                   shadow-sm transition duration-300">
+        Learn more
+    </button>
+    
+    </div>
+</a>
+</div>
+        
+        <div className="relative md:w-6/12 w-full overflow-hidden">
+        
+            <a href="https://www.apple.com/ipad-air/">
+        
+            <img src={fff} className="w-full h-100 object-cover  flex mt-2" />
+            </a>
+           
+            <a href="https://www.apple.com/ipad-air/">
+        
+            <div className=''>
+        
+        <span className='absolute flex top-9 justify-center left-0 right-1
+                          text-4xl font-bold
+        '>iPad Air</span>
+        
+        <span className='absolute flex top-20 items-center justify-center left-0 right-1
+                          text-xl 
+        '>Now superccharged by M4</span>
+            <button className="absolute flex top-30 justify-center right-1/2
+                           bg-blue-500 hover:bg-blue-400 
+                           text-white font-bold py-2 px-4 rounded-3xl 
+                           shadow-lg transition duration-300">
+                Learn more
+            </button>
+            <button className="absolute top-30 justify-center left-1/2 
+                           bg-white hover:bg-blue-500 hover:text-white
+                           text-blue-500 font-bold py-2 px-4 rounded-3xl round-
+                           shadow-sm transition duration-200">
+                Buy
+            </button>
+            </div>
+        </a>
+        
+        </div>
+
+
+<div className="relative md:w-6/12 w-full overflow-hidden">
+
+    <a href="https://www.apple.com/macbook-pro/ ">
+
+    <img src={ss} className="w-full h-100 object-cover  flex mt-2" />
+    </a>
+   
+<a href="https://www.apple.com/macbook-air/">
+
+    <div className=''>
+
+<span className='absolute flex top-9 justify-center left-0 right-1
+                  text-4xl font-bold text-white
+'>MacBook Pro</span>
+
+<span className='absolute flex top-20 items-center justify-center left-0 right-1
+                  text-xl text-white
+'>Now with M5,M5 Pro,and M5 Max.</span>
+    <button className="absolute flex top-30 justify-center right-1/2
+                   bg-blue-500 hover:bg-blue-400 
+                   text-white font-bold py-2 px-4 rounded-3xl 
+                   shadow-lg transition duration-300">
+        Learn more
+    </button>
+    <button className="absolute top-30 justify-center left-1/2 
+                   bg-white hover:bg-blue-500 hover:text-white
+                   text-blue-500 font-bold py-2 px-4 rounded-3xl round-
+                   shadow-sm transition duration-200">
+        Buy
+    </button>
+    </div>
+</a>
+
+</div>
+
+
+
+<div className="relative md:w-6/12 w-full overflow-hidden">
+
+    <a href="https://www.apple.com/apple-watch-series-11/ ">
+
+    <img src={sss} className="w-full h-100 object-cover  flex mt-2" />
+    </a>
+   
+    <a href="https://www.apple.com/apple-watch-series-11/ ">
+
+    <div className=''>
+
+<span className='absolute flex top-9 justify-center left-0 right-1
+                  text-4xl font-bold
+'><box-icon type='logo' name='apple'></box-icon>
+
+    WATCH SERIES11</span>
+
+<span className='absolute flex top-20 items-center justify-center left-0 right-1
+                  text-xl 
+'>The ultimate way to watch your health.</span>
+    <button className="absolute flex top-30 justify-center right-1/2
+                   bg-blue-500 hover:bg-blue-400 
+                   text-white font-bold py-2 px-4 rounded-3xl 
+                   shadow-sm transition duration-300">
+        Learn more
+    </button>
+    <button className="absolute top-30 justify-center left-1/2 
+                   bg-white hover:bg-blue-500 hover:text-white
+                   text-blue-500 font-bold py-2 px-4 rounded-3xl round-
+                   shadow-sm transition duration-200">
+        Buy
+    </button>
+    </div>
+</a>
+</div>
+
+
+<div className="relative md:w-6/12 w-full overflow-hidden">
+
+    <a href="https://www.apple.com/us/shop/goto/trade_in ">
+
+    <img src={e} className="w-full h-100 object-cover  flex mt-2" />
+    </a>
+   
+    <a href="https://www.apple.com/us/shop/goto/trade_in ">
+
+    <div className=''>
+
+<span className='absolute flex top-9 justify-center left-0 right-1
+                  text-4xl font-bold
+'><box-icon type='logo' name='apple'></box-icon>
+
+    Trade In</span>
+
+<span className='absolute flex top-20 items-center justify-center left-0 right-1
+                  text-xl 
+'>
+
+Get up tp$195-$695
+ in credit when you
+  trade in iPhone
+   131 or higher.</span>
+    <button className="absolute flex top-30 justify-center right-1/2
+                   bg-blue-500 hover:bg-blue-400 
+                   text-white font-bold py-2 px-4 rounded-3xl 
+                   shadow-sm transition duration-300">
+        Get your estimate
+    </button>
+    
+    </div>
+</a>
+</div>
+
+
+<div className="relative md:w-6/12 w-full overflow-hidden">
+
+<a href="https://www.apple.com/apple-card/ ">
+
+    <img src={n} className="w-full h-100 object-cover  flex mt-2" />
+    </a>
+   
+<a href="https://www.apple.com/apple-card/ ">
+
+    <div className=''>
+
+<span className='absolute flex top-9 justify-center left-0 right-1
+                  text-4xl font-bold
+'><box-icon type='logo' name='apple'></box-icon>
+
+    Card</span>
+
+<span className='absolute flex top-20 items-center justify-center left-0 right-1
+                  text-xl 
+'>
+
+Get up tp 3% Daily Cash back with every purchase</span>
+    <button className="absolute flex top-30 justify-center right-1/2
+                   bg-blue-500 hover:bg-blue-400 
+                   text-white font-bold py-2 px-4 rounded-3xl 
+                   shadow-sm transition duration-300">
+        Learn more
+    </button>
+    <button className="absolute flex top-30 justify-center right-1/2
+                   bg-blue-500 hover:bg-blue-400 
+                   text-white font-bold py-2 px-4 rounded-3xl 
+                   shadow-sm transition duration-300">
+        Apply now
+    </button>
+    
+    </div>
+</a>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </section>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<section className='container-fluid'>
+    <section className='flex flex-wrap'>
+
+
+
+<h1 className='text-5xl mt-8 flex justify-center items-center
+
+
+
+'>
+    Endless entertainment.
+</h1>
+
+
+
+
+
+
+
+
+    </section>
+</section>
+
+
+
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default main
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Header from './Components/Header'
+import Main2 from './Components/Main2'
+function App() {
+ 
+
+  return (
+    <>
+     <Header/>
+     <Main2/>
+    </>
+  )
+}
+
+export default App
